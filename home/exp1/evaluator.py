de19d4124e841fa0cfccb3881e1a6f79e7521695
Arthur Tofani <gramofone@gmail.com>
2020-07-20 00:38:34 -0300

    receives any field name

--- home/exp1/evaluator.py
+++ home/exp1/evaluator.py
@@ -17,7 +17,7 @@ class Evaluator:
   project = None
   match_results = None
   ranking_size = None
-  track_frequencies = None
+  freqs = None
   results = None
 
   def __init__(self, project):
@@ -27,19 +27,21 @@ class Evaluator:
   def build(self, dataset):
     logging.info('Building dataset')
     self.dataset = dataset
+    amnt = len(self.dataset.session.unique())
     for idx, session in enumerate(self.dataset.session.unique()):
-      logging.info('Adding session %s' % session)
+      logging.info('Adding session %s/%s' % (idx, amnt))
       self.project.add(str(session))
 
 
-  def match(self, query_dataset, amnt_results_per_query=10, top_n_tracks=10):
+  def match(self, field_name, query_dataset, amnt_results_per_query=10, top_n_recs=10):
     '''
     Performs match between the songs in the query_files file and those ones
     received by the #build method
     '''
     df = self.query_dataset = query_dataset
+    amnt = len(df.session.unique())
     for idx, session in enumerate(df.session.unique()):
-      logging.info('matching session %s' % session)
+      logging.info('Matching session %s/%s' % (idx, amnt))
       query = df[df.session==int(session)]
 
       # Perform query on database
@@ -47,38 +49,38 @@ class Evaluator:
 
 
       # Use the returned session ids to gather session info on self.dataset
-      returned_tracks = self.dataset[self.dataset.session.isin([i.filename for i in payload])]
-      returned_tracks = returned_tracks[~returned_tracks.track.isin(query.track)] # remove songs already in query session
+      returned_records = self.dataset[self.dataset.session.isin([i.filename for i in payload])]
+      returned_records = returned_records[~returned_records[field_name].isin(query[field_name])] # remove songs already in query session
 
-      # Calculate the most frequent tracks associated to that query session (but not in the session)
-      track_frequencies = returned_tracks.groupby(['track']).count()
-      track_frequencies = track_frequencies.reset_index()
-      track_frequencies['track_num'] = track_frequencies['track'].astype(int)
-      track_frequencies = track_frequencies.sort_values(['session', 'track_num'], ascending=[False, True])
-      track_frequencies = track_frequencies[['track', 'user']]
-      track_frequencies.columns = ['track', 'count']
-      track_frequencies = track_frequencies[:top_n_tracks]
-      track_frequencies['query_session'] = session
-      if self.track_frequencies is None:
-        self.track_frequencies = track_frequencies
+      # Calculate the most frequent recs associated to that query session (but not in the session)
+      freqs = returned_records.groupby([field_name]).count()
+      freqs = freqs.reset_index()
+      freqs['track_num'] = freqs[field_name].astype(int)
+      freqs = freqs.sort_values(['session', 'track_num'], ascending=[False, True])
+      freqs = freqs[[field_name, 'user']]
+      freqs.columns = [field_name, 'count']
+      freqs = freqs[:top_n_recs]
+      freqs['query_session'] = session
+      if self.freqs is None:
+        self.freqs = freqs
       else:
-        self.track_frequencies = pd.concat([self.track_frequencies, track_frequencies])
+        self.freqs = pd.concat([self.freqs, freqs])
 
-  def evaluate(self, query_dataset_full):
-    self.results = pd.DataFrame(columns=['session', 'returned_candidates', 'unique_candidates', 'top1_match', 'top_n_match', 'terms_in_session', 'candidate_tracks', 'rank_ratio'])
+  def evaluate(self, field_name, query_dataset_full):
+    self.results = pd.DataFrame(columns=['session', 'returned_candidates', 'unique_candidates', 'top1_match', 'top_n_match', 'terms_in_session', 'candidate_recs', 'rank_ratio'])
     for idx, session in enumerate(self.query_dataset.session.unique()):
       full_session = query_dataset_full[query_dataset_full.session==int(session)]
 
       # The songs recommended by the system
       query = self.query_dataset[self.query_dataset.session==session]
-      tracks = self.track_frequencies[self.track_frequencies.query_session==session]
-      tracks_present_top_1 = tracks.track[:1].isin(full_session.track).astype(int).sum()
-      tracks_present_top_n = tracks.track.isin(full_session.track).astype(int).sum()
+      recs = self.freqs[self.freqs.query_session==session]
+      recs_present_top_1 = recs[field_name][:1].isin(full_session[field_name]).astype(int).sum()
+      recs_present_top_n = recs[field_name].isin(full_session[field_name]).astype(int).sum()
       terms_in_query = len(query)
-      candidate_tracks = len(tracks)
+      candidate_recs = len(recs)
       try:
-        rank_ratio = tracks_present_top_n/candidate_tracks
+        rank_ratio = recs_present_top_n/candidate_recs
       except:
         rank_ratio = 0
       #import code; code.interact(local=dict(globals(), **locals()))
-      self.results.loc[len(self.results)] = ([session, tracks['count'].sum(), len(tracks), tracks_present_top_1, tracks_present_top_n, terms_in_query, candidate_tracks, rank_ratio])
+      self.results.loc[len(self.results)] = ([session, recs['count'].sum(), len(recs), recs_present_top_1, recs_present_top_n, terms_in_query, candidate_recs, rank_ratio])
--- home/main.py
+++ home/main.py
@@ -1,6 +1,6 @@
 import time
 from multiprocessing import Pool
-from itertools import chain, combinations
+from itertools import chain, combinations, product
 from footprint.models import Project
 import os
 import logging
@@ -23,9 +23,11 @@ logging.basicConfig(
 logging.info("Starting evaluation")
 db_index_name = 'recommendation_exp1'
 dataset = pd.read_csv('/dataset/sessions.md.csv')
+field_name = 'track'
 
 ds_counts = dataset.groupby(['session']).count()
-cond = (ds_counts.track >= 9) & (ds_counts.track <= 15)
+cond = (ds_counts['track'] >= 9) & (ds_counts['track'] <= 15)
+cond = (ds_counts['track'] >= 2)
 dataset = dataset[dataset.session.isin(ds_counts[cond].index)]
 
 msk = np.random.rand(len(dataset.session.unique())) < 0.8
@@ -35,35 +37,40 @@ query_dataset_full = dataset[~dataset.session.isin(dataset.session.unique()[msk]
 #train_dataset = dataset[dataset.session.isin(dataset.session.unique()[msk])]
 #query_dataset_full = dataset[~dataset.session.isin(dataset.session.unique()[msk])]
 
-tracks_to_remove = query_dataset_full.groupby(['session']).apply(lambda x: x.sample(int(len(x)/2))).track
+recs_to_remove = query_dataset_full.groupby(['session']).apply(lambda x: x.sample(int(len(x)/2)))[field_name]
 ds_counts = dataset.groupby(['session']).count()
 
 # create a new dataset by removing 1 track (randomly) from each session
-query_dataset = query_dataset_full[~query_dataset_full.track.isin(tracks_to_remove)].copy().reset_index()
-#query_dataset = query_dataset[~query_dataset.track.isin(tracks_to_remove)].copy().reset_index()
-query_dataset
+query_dataset = query_dataset_full[~query_dataset_full[field_name].isin(recs_to_remove)].copy().reset_index()
+#query_dataset = query_dataset[~query_dataset[field_name].isin(tracks_to_remove)].copy().reset_index()
 
 
-def session_tracks(document):
-  tokens = dataset[dataset.session==int(document.filename)].track.astype(str)
+
+def get_terms(document):
+  tokens = dataset[dataset.session==int(document.filename)][field_name].astype(str)
   return ' '.join(tokens)
 
-def session_artists(document):
-  tokens = dataset[dataset.session==int(document.filename)].artist.astype(str)
+def get_terms_with_seq(document):
+  r = dataset[dataset.session==int(document.filename)][field_name].astype(str)
+  r2 = list(r[1:]) + ['-']
+  tokens = list(r + ':' + r2)
   return ' '.join(tokens)
+
 
 # Initialize project, db instance and evaluator
 project = Project()
 db.connect_to_elasticsearch(project, db_index_name, True)
-project.client.set_scope(db_index_name, ['tracks'], 'tokens_by_spaces')
+project.client.set_scope(db_index_name, [field_name], 'tokens_by_spaces')
 evaluator = Evaluator(project)
 
-project.tokenize('tracks', session_tracks)
+project.tokenize(field_name, get_terms_with_seq)
 #project.tokenize('tracks', session_artists)
 
 evaluator.build(train_dataset)
-evaluator.match(query_dataset)
-evaluator.evaluate(query_dataset_full)
+evaluator.match(field_name, query_dataset)
+evaluator.evaluate(field_name, query_dataset_full)
+print('Train dataset: ', len(train_dataset.session.unique()), 'sessions')
+print('Query dataset: ', len(query_dataset.session.unique()), 'sessions')
 print(evaluator.results.mean())
 
 
