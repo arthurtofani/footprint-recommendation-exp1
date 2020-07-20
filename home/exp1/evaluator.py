b86f231eee3877aedc402879212cc1a10fd6aef2
Arthur Tofani <gramofone@gmail.com>
2020-07-19 23:17:50 -0300

    working

--- home/exp1/evaluator.py
+++ home/exp1/evaluator.py
@@ -1,8 +1,9 @@
-import numpy as np
+
 import glob
 import random
 from collections import defaultdict
 import pandas as pd
+import numpy as np
 import sklearn
 import os.path
 from footprint import util as util
@@ -11,72 +12,73 @@ import csv
 import logging
 
 class Evaluator:
-  records_list_path = None
-  db_files = None
-  query_files = None
+  dataset = None
+  query_dataset = None
   project = None
   match_results = None
   ranking_size = None
-  distance_matrix = None
+  track_frequencies = None
+  results = None
 
   def __init__(self, project):
     self.project = project
-    self.distance_matrix = None
+    self.results = None
 
   def build(self, dataset):
     logging.info('Building dataset')
-    df = pd.read_csv(dataset)
-    for idx, session in enumerate(df.session.unique()):
+    self.dataset = dataset
+    for idx, session in enumerate(self.dataset.session.unique()):
       logging.info('Adding session %s' % session)
       self.project.add(str(session))
 
 
-  def match(self, query_files, amnt_results_per_query=10):
+  def match(self, query_dataset, amnt_results_per_query=10, top_n_tracks=10):
     '''
     Performs match between the songs in the query_files file and those ones
     received by the #build method
     '''
-    df = pd.read_csv(query_files)
-
-    dataset = '/dataset/sessions.sm.csv'
-    query_datasets = '/dataset/sessions_eval.sm.csv'
-    df  = pd.read_csv(dataset)
-    df2 = pd.read_csv(query_datasets)
-
+    df = self.query_dataset = query_dataset
     for idx, session in enumerate(df.session.unique()):
       logging.info('matching session %s' % session)
-      res = self.project.match(str(session), amnt_results_per_query)
+      query = df[df.session==int(session)]
 
-      query = df2[df2.session==int(session)]
-      results = [(i.filename, i.score) for i in res[1]]
-
-      import code; code.interact(local=dict(globals(), **locals()))
-      x = df[df.session.isin([i.filename for i in res[1]])]
-      x.groupby(['track']).count().sort_values(['user'], ascending=False).user
+      # Perform query on database
+      payload = self.project.match(str(session), amnt_results_per_query)[1]
 
 
-      def check(xx):
-        return df[df.session==int(xx)]
+      # Use the returned session ids to gather session info on self.dataset
+      returned_tracks = self.dataset[self.dataset.session.isin([i.filename for i in payload])]
+      returned_tracks = returned_tracks[~returned_tracks.track.isin(query.track)] # remove songs already in query session
 
+      # Calculate the most frequent tracks associated to that query session (but not in the session)
+      track_frequencies = returned_tracks.groupby(['track']).count()
+      track_frequencies = track_frequencies.reset_index()
+      track_frequencies['track_num'] = track_frequencies['track'].astype(int)
+      track_frequencies = track_frequencies.sort_values(['session', 'track_num'], ascending=[False, True])
+      track_frequencies = track_frequencies[['track', 'user']]
+      track_frequencies.columns = ['track', 'count']
+      track_frequencies = track_frequencies[:top_n_tracks]
+      track_frequencies['query_session'] = session
+      if self.track_frequencies is None:
+        self.track_frequencies = track_frequencies
+      else:
+        self.track_frequencies = pd.concat([self.track_frequencies, track_frequencies])
 
+  def evaluate(self, query_dataset_full):
+    self.results = pd.DataFrame(columns=['session', 'returned_candidates', 'unique_candidates', 'top1_match', 'top_n_match', 'terms_in_session', 'candidate_tracks', 'rank_ratio'])
+    for idx, session in enumerate(self.query_dataset.session.unique()):
+      full_session = query_dataset_full[query_dataset_full.session==int(session)]
 
-    #    ct = 0
-    #    self.query_files = query_files
-    #    self.match_results = []
-    #    self.amnt_results_per_query = amnt_results_per_query
-    #    self.queries = self.get_filenames(self.query_files)
-    #
-    #    sz = len(self.queries)
-    #    for f in self.queries:
-    #      ct+=1
-    #      print('%s of %s => %s' % (ct, sz, f))
-    #      res = self.project.match(f, self.amnt_results_per_query)
-    #      #yield res
-    #      res[0].features = dict()
-    #      self.match_results.append(res)
-
-  def evaluate(self):
-    pass
-
-  def results(self, clique_map, ranking_size=10):
-    pass
+      # The songs recommended by the system
+      query = self.query_dataset[self.query_dataset.session==session]
+      tracks = self.track_frequencies[self.track_frequencies.query_session==session]
+      tracks_present_top_1 = tracks.track[:1].isin(full_session.track).astype(int).sum()
+      tracks_present_top_n = tracks.track.isin(full_session.track).astype(int).sum()
+      terms_in_query = len(query)
+      candidate_tracks = len(tracks)
+      try:
+        rank_ratio = tracks_present_top_n/candidate_tracks
+      except:
+        rank_ratio = 0
+      #import code; code.interact(local=dict(globals(), **locals()))
+      self.results.loc[len(self.results)] = ([session, tracks['count'].sum(), len(tracks), tracks_present_top_1, tracks_present_top_n, terms_in_query, candidate_tracks, rank_ratio])
--- home/main.py
+++ home/main.py
@@ -22,16 +22,34 @@ logging.basicConfig(
 
 logging.info("Starting evaluation")
 db_index_name = 'recommendation_exp1'
-dataset = '/dataset/sessions.sm.csv'
-query_datasets = '/dataset/sessions_eval.sm.csv'
-df = pd.read_csv(dataset)
-df2 = pd.read_csv(query_datasets)
+dataset = pd.read_csv('/dataset/sessions.md.csv')
+
+ds_counts = dataset.groupby(['session']).count()
+cond = (ds_counts.track >= 9) & (ds_counts.track <= 15)
+dataset = dataset[dataset.session.isin(ds_counts[cond].index)]
+
+msk = np.random.rand(len(dataset.session.unique())) < 0.8
+train_dataset = dataset[dataset.session.isin(dataset.session.unique()[msk])]
+query_dataset_full = dataset[~dataset.session.isin(dataset.session.unique()[msk])]
+
+#train_dataset = dataset[dataset.session.isin(dataset.session.unique()[msk])]
+#query_dataset_full = dataset[~dataset.session.isin(dataset.session.unique()[msk])]
+
+tracks_to_remove = query_dataset_full.groupby(['session']).apply(lambda x: x.sample(int(len(x)/2))).track
+ds_counts = dataset.groupby(['session']).count()
+
+# create a new dataset by removing 1 track (randomly) from each session
+query_dataset = query_dataset_full[~query_dataset_full.track.isin(tracks_to_remove)].copy().reset_index()
+#query_dataset = query_dataset[~query_dataset.track.isin(tracks_to_remove)].copy().reset_index()
+query_dataset
+
+
 def session_tracks(document):
-  tokens = df[df.session==int(document.filename)].track.astype(str)
+  tokens = dataset[dataset.session==int(document.filename)].track.astype(str)
   return ' '.join(tokens)
 
 def session_artists(document):
-  tokens = df[df.session==int(document.filename)].artist.astype(str)
+  tokens = dataset[dataset.session==int(document.filename)].artist.astype(str)
   return ' '.join(tokens)
 
 # Initialize project, db instance and evaluator
@@ -43,8 +61,11 @@ evaluator = Evaluator(project)
 project.tokenize('tracks', session_tracks)
 #project.tokenize('tracks', session_artists)
 
-evaluator.build(dataset)
-evaluator.match(query_datasets)
+evaluator.build(train_dataset)
+evaluator.match(query_dataset)
+evaluator.evaluate(query_dataset_full)
+print(evaluator.results.mean())
+
 
 #import code; code.interact(local=dict(globals(), **locals()))
 
@@ -57,4 +78,4 @@ evaluator.match(query_datasets)
 
 #keys = [k for k in p.tokenization_methods.keys() if k not in already_processed_keys]
 
-#import code; code.interact(local=dict(globals(), **locals()))
+import code; code.interact(local=dict(globals(), **locals()))
