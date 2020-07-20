7ae0916c6d2cde917648280497e5b4559cb10a89
Arthur Tofani <gramofone@gmail.com>
2020-07-20 11:07:51 -0300

    some improvements

--- home/main.py
+++ home/main.py
@@ -18,34 +18,6 @@ logging.basicConfig(
     ]
 )
 
-
-
-logging.info("Starting evaluation")
-db_index_name = 'recommendation_exp1'
-dataset = pd.read_csv('/dataset/sessions.md.csv')
-field_name = 'track'
-
-ds_counts = dataset.groupby(['session']).count()
-cond = (ds_counts['track'] >= 9) & (ds_counts['track'] <= 15)
-cond = (ds_counts['track'] >= 2)
-dataset = dataset[dataset.session.isin(ds_counts[cond].index)]
-
-msk = np.random.rand(len(dataset.session.unique())) < 0.8
-train_dataset = dataset[dataset.session.isin(dataset.session.unique()[msk])]
-query_dataset_full = dataset[~dataset.session.isin(dataset.session.unique()[msk])]
-
-#train_dataset = dataset[dataset.session.isin(dataset.session.unique()[msk])]
-#query_dataset_full = dataset[~dataset.session.isin(dataset.session.unique()[msk])]
-
-recs_to_remove = query_dataset_full.groupby(['session']).apply(lambda x: x.sample(int(len(x)/2)))[field_name]
-ds_counts = dataset.groupby(['session']).count()
-
-# create a new dataset by removing 1 track (randomly) from each session
-query_dataset = query_dataset_full[~query_dataset_full[field_name].isin(recs_to_remove)].copy().reset_index()
-#query_dataset = query_dataset[~query_dataset[field_name].isin(tracks_to_remove)].copy().reset_index()
-
-
-
 def get_terms(document):
   tokens = dataset[dataset.session==int(document.filename)][field_name].astype(str)
   return ' '.join(tokens)
@@ -56,6 +28,30 @@ def get_terms_with_seq(document):
   tokens = list(r + ':' + r2)
   return ' '.join(tokens)
 
+
+logging.info("Starting evaluation")
+db_index_name = 'recommendation_exp1'
+dataset = pd.read_csv('/dataset/sessions.sm.csv')
+field_name = 'track'
+
+ds_counts = dataset.groupby(['session']).count()
+#cond = (ds_counts['track'] >= 9) & (ds_counts['track'] <= 15)
+cond = (ds_counts['track'] >= 2)
+dataset = dataset[dataset.session.isin(ds_counts[cond].index)]
+
+msk = np.random.rand(len(dataset.session.unique())) < 0.8
+train_dataset = dataset[dataset.session.isin(dataset.session.unique()[msk])]
+query_dataset_full = dataset[~dataset.session.isin(dataset.session.unique()[msk])]
+
+final_df = DataFrame(columns=['iter', 'num_train_sessions',
+                              'num_test_sessions', 'query_session_size',
+                              'top1_acc'])
+
+recs_to_remove = query_dataset_full.groupby(['session']).apply(lambda x: x.sample(int(len(x)/2)))[field_name]
+ds_counts = dataset.groupby(['session']).count()
+
+# create a new dataset by removing 1 track (randomly) from each session
+query_dataset = query_dataset_full[~query_dataset_full[field_name].isin(recs_to_remove)].copy().reset_index()
 
 # Initialize project, db instance and evaluator
 project = Project()
@@ -74,15 +70,5 @@ print('Query dataset: ', len(query_dataset.session.unique()), 'sessions')
 print(evaluator.results.mean())
 
 
-#import code; code.interact(local=dict(globals(), **locals()))
-
-#p.process_feature('track_ids', features.session_tracks)
-
-#max_processors = 3
-
-#evaluator = evaluators.CSI(p)
-
-
-#keys = [k for k in p.tokenization_methods.keys() if k not in already_processed_keys]
 
 import code; code.interact(local=dict(globals(), **locals()))
