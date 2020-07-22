08c23ab733d02cc38abe8d6eaaa5f19d5d3519e4
Arthur Tofani <gramofone@gmail.com>
2020-07-21 21:41:42 -0300

    iterations

--- home/exp1/evaluator.py
+++ home/exp1/evaluator.py
@@ -23,59 +23,59 @@ class Evaluator:
     self.project = project
     self.results = None
 
-  def build(self, dataset):
+  def build(self, document_field, dataset):
     logging.info('Building dataset')
     self.dataset = dataset
-    amnt = len(self.dataset.session.unique())
-    for idx, session in enumerate(self.dataset.session.unique()):
-      logging.info('Adding session %s/%s' % (idx, amnt))
-      self.project.add(str(session))
+    amnt = len(self.dataset[document_field].unique())
+    for idx, document in enumerate(self.dataset[document_field].unique()):
+      logging.info('Adding document %s/%s' % (idx, amnt))
+      self.project.add(str(document))
 
 
-  def match(self, field_name, query_dataset, amnt_results_per_query=10, top_n_recs=10):
+  def match(self, document_field, term_field, query_dataset, amnt_results_per_query=10, top_n_recs=10):
     '''
     Performs match between the songs in the query_files file and those ones
     received by the #build method
     '''
     df = self.query_dataset = query_dataset
-    amnt = len(df.session.unique())
-    for idx, session in enumerate(df.session.unique()):
-      logging.info('Matching session %s/%s' % (idx, amnt))
-      query = df[df.session==int(session)]
+    amnt = len(df[document_field].unique())
+    for idx, document in enumerate(df[document_field].unique()):
+      logging.info('Matching document %s/%s' % (idx, amnt))
+      query = df[df[document_field]==int(document)]
 
       # Perform query on database
-      payload = self.project.match(str(session), amnt_results_per_query)[1]
+      payload = self.project.match(str(document), amnt_results_per_query)[1]
 
 
-      # Use the returned session ids to gather session info on self.dataset
-      returned_records = self.dataset[self.dataset.session.isin([i.filename for i in payload])]
-      returned_records = returned_records[~returned_records[field_name].isin(query[field_name])] # remove songs already in query session
+      # Use the returned document ids to gather document info on self.dataset
+      returned_records = self.dataset[self.dataset[document_field].isin([i.filename for i in payload])]
+      returned_records = returned_records[~returned_records[term_field].isin(query[term_field])] # remove songs already in query document
 
-      # Calculate the most frequent recs associated to that query session (but not in the session)
-      freqs = returned_records.groupby([field_name]).count()
+      # Calculate the most frequent recs associated to that query document (but not in the document)
+      freqs = returned_records.groupby([term_field]).count()
       freqs = freqs.reset_index()
-      freqs['track_num'] = freqs[field_name].astype(int)
-      freqs = freqs.sort_values(['session', 'track_num'], ascending=[False, True])
-      freqs = freqs[[field_name, 'user']]
-      freqs.columns = [field_name, 'count']
+      freqs['track_num'] = freqs[term_field].astype(int)
+      freqs = freqs.sort_values([document_field, 'track_num'], ascending=[False, True])
+      freqs = freqs[[term_field, 'user']]
+      freqs.columns = [term_field, 'count']
       freqs = freqs[:top_n_recs]
-      freqs['query_session'] = session
+      freqs['query_document'] = document
       if self.freqs is None:
         self.freqs = freqs
       else:
         self.freqs = pd.concat([self.freqs, freqs])
 
-  def evaluate(self, field_name, query_dataset_full):
-    self.results = pd.DataFrame(columns=['session', 'returned_candidates', 'unique_candidates', 'top1_match', 'top_5_match', 'top_10_match', 'terms_in_session', 'candidate_recs', 'rank_ratio_5', 'rank_ratio_10'])
-    for idx, session in enumerate(self.query_dataset.session.unique()):
-      full_session = query_dataset_full[query_dataset_full.session==int(session)]
+  def evaluate(self, document_field, term_field, query_dataset_full):
+    self.results = pd.DataFrame(columns=[document_field, 'returned_candidates', 'unique_candidates', 'top1_match', 'top_5_match', 'top_10_match', 'terms_in_document', 'candidate_recs', 'rank_ratio_5', 'rank_ratio_10'])
+    for idx, document in enumerate(self.query_dataset[document_field].unique()):
+      full_document = query_dataset_full[query_dataset_full[document_field]==int(document)]
 
       # The songs recommended by the system
-      query = self.query_dataset[self.query_dataset.session==session]
-      recs = self.freqs[self.freqs.query_session==session]
-      recs_present_top_1 = recs[field_name][:1].isin(full_session[field_name]).astype(int).sum()
-      recs_present_top_5 = recs[field_name][:5].isin(full_session[field_name]).astype(int).sum()
-      recs_present_top_10 = recs[field_name][:10].isin(full_session[field_name]).astype(int).sum()
+      query = self.query_dataset[self.query_dataset[document_field]==document]
+      recs = self.freqs[self.freqs.query_document==document]
+      recs_present_top_1 = recs[term_field][:1].isin(full_document[term_field]).astype(int).sum()
+      recs_present_top_5 = recs[term_field][:5].isin(full_document[term_field]).astype(int).sum()
+      recs_present_top_10 = recs[term_field][:10].isin(full_document[term_field]).astype(int).sum()
       terms_in_query = len(query)
       candidate_recs = len(recs)
       try:
@@ -85,6 +85,6 @@ class Evaluator:
         rank_ratio_5 = 0
         rank_ratio_10 = 0
       #import code; code.interact(local=dict(globals(), **locals()))
-      xx = [session,    recs['count'].sum(),  len(recs),           recs_present_top_1, recs_present_top_5, recs_present_top_10, terms_in_query,    candidate_recs,   rank_ratio_5, rank_ratio_10]
+      xx = [document,    recs['count'].sum(),  len(recs),           recs_present_top_1, recs_present_top_5, recs_present_top_10, terms_in_query,    candidate_recs,   rank_ratio_5, rank_ratio_10]
 
       self.results.loc[len(self.results)] = (xx)
--- home/main.py
+++ home/main.py
@@ -19,75 +19,104 @@ logging.basicConfig(
 )
 
 def get_terms(document):
-  tokens = dataset[dataset.session==int(document.filename)][field_name].astype(str)
+  tokens = dataset[dataset[document_field]==int(document.filename)][term_field].astype(str)
   return ' '.join(tokens)
 
 def get_terms_with_seq(document):
-  r = dataset[dataset.session==int(document.filename)][field_name].astype(str)
+  r = dataset[dataset[document_field]==int(document.filename)][term_field].astype(str)
   r2 = list(r[1:]) + ['-']
   tokens = list(r + ':' + r2)
   return ' '.join(tokens)
 
+final_df = pd.DataFrame(columns=['iter', 'num_query_terms', 'document_field', 'term_field', 'strgy',
+                              'top1_acc', 'top5_acc', 'top10_acc',
+                              'num_train_documents', 'num_test_documents', 'query_terms_ratio'])
 
+output_file = '/notebook/all_results.csv'
+num_iterations = 3
 logging.info("Starting evaluation")
 db_index_name = 'recommendation_exp1'
-#dataset = pd.read_csv('/dataset/sessions.md.csv')
-dataset = pd.read_csv('/dataset/pcounts_filtered_december_2013.csv')
+dataset = pd.read_csv('/dataset/sessions.md.csv')
 
-field_name = 'track'
+#dataset = pd.read_csv('/dataset/pcounts_filtered_december_2013.csv')
 
-ds_counts = dataset.groupby(['session']).count()
-#cond = (ds_counts['track'] >= 9) & (ds_counts['track'] <= 15)
-cond = (ds_counts['track'] >= 2)
-dataset = dataset[dataset.session.isin(ds_counts[cond].index)]
+for document_field in ['session', 'user']:
+  for term_field in ['track', 'artist']:
+    for tokenization_method in [get_terms, get_terms_with_seq]:
+      for query_terms_ratio in [0.2, 0.4, 0.5, 0.6, 0.8]:
+        for itr in range(num_iterations):
 
-factor = 0.8
-sessions = dataset.sort_values(['timestamp']).session.unique()
-train_sessions = sessions[0:int(len(sessions) * factor)]
-test_sessions = sessions[int(len(sessions) * factor):]
+          ds_counts = dataset.groupby([document_field]).count()
+          #cond = (ds_counts['track'] >= 9) & (ds_counts['track'] <= 15)
+          cond = (ds_counts[term_field] >= 2)
+          dataset = dataset[dataset[document_field].isin(ds_counts[cond].index)]
 
-train_dataset = dataset[dataset.session.isin(train_sessions)]
-query_dataset_full = dataset[dataset.session.isin(test_sessions)]
+          factor = 0.8
+
+          documents = dataset.sort_values(['time'])[document_field].unique()
+          train_documents = documents[0:int(len(documents) * factor)]
+          test_documents = documents[int(len(documents) * factor):]
+
+          train_dataset = dataset[dataset[document_field].isin(train_documents)]
+          query_dataset_full = dataset[dataset[document_field].isin(test_documents)]
 
 
 
-#msk = np.random.rand(len(dataset.session.unique())) < 0.8
-#train_dataset = dataset[dataset.session.isin(dataset.session.unique()[msk])]
-#query_dataset_full = dataset[~dataset.session.isin(dataset.session.unique()[msk])]
-
-
-
-#final_df = DataFrame(columns=['iter', 'num_train_sessions',
-#                              'num_test_sessions', 'query_session_size',
-#                              'top1_acc'])
-
-recs_to_remove = query_dataset_full.groupby(['session']).apply(lambda x: x.sample(int(len(x)*0.8)))[field_name]
-ds_counts = dataset.groupby(['session']).count()
-
-# create a new dataset by removing 1 track (randomly) from each session
-query_dataset = query_dataset_full[~query_dataset_full[field_name].isin(recs_to_remove)].copy().reset_index()
+          #msk = np.random.rand(len(dataset[document_field].unique())) < 0.8
+          #train_dataset = dataset[dataset[document_field].isin(dataset[document_field].unique()[msk])]
+          #query_dataset_full = dataset[~dataset[document_field].isin(dataset[document_field].unique()[msk])]
 
 
 
 
-# Initialize project, db instance and evaluator
-project = Project()
-db.connect_to_elasticsearch(project, db_index_name, True)
-project.client.set_scope(db_index_name, [field_name], 'tokens_by_spaces')
-evaluator = Evaluator(project)
+          recs_to_remove = query_dataset_full.groupby([document_field]).apply(lambda x: x.sample(int(len(x)*query_terms_ratio)))[term_field]
+          ds_counts = dataset.groupby([document_field]).count()
 
-project.tokenize(field_name, get_terms_with_seq)
-#project.tokenize('tracks', session_artists)
-
-evaluator.build(train_dataset)
-evaluator.match(field_name, query_dataset)
-evaluator.evaluate(field_name, query_dataset_full)
-print('Train dataset: ', len(train_dataset.session.unique()), 'sessions')
-print('Query dataset: ', len(query_dataset.session.unique()), 'sessions')
-print(evaluator.results.mean())
+          # create a new dataset by removing 1 track (randomly) from each session
+          query_dataset = query_dataset_full[~query_dataset_full[term_field].isin(recs_to_remove)].copy().reset_index()
 
 
 
+
+          # Initialize project, db instance and evaluator
+          project = Project()
+          db.connect_to_elasticsearch(project, db_index_name, True)
+          project.client.set_scope(db_index_name, [term_field], 'tokens_by_spaces')
+          evaluator = Evaluator(project)
+
+          project.tokenize(term_field, tokenization_method)
+          #project.tokenize('tracks', session_artists)
+
+          evaluator.build(document_field, train_dataset)
+          evaluator.match(document_field, term_field, query_dataset)
+          evaluator.evaluate(document_field, term_field, query_dataset_full)
+          print('Train dataset: ', len(train_dataset[document_field].unique()), 'documents')
+          print('Query dataset: ', len(query_dataset[document_field].unique()), 'documents')
+          print(evaluator.results.mean())
+
+          complete_row = [itr, 0, document_field, term_field,
+            tokenization_method.__name__, evaluator.results.mean().top1_match, evaluator.results.mean().top_5_match,
+            evaluator.results.mean().top_10_match, len(train_dataset),
+            len(query_dataset_full), query_terms_ratio]
+
+          final_df.loc[len(final_df)] = complete_row
+
+          for i, r in evaluator.results.groupby(['terms_in_document']).mean().iterrows():
+            #import code; code.interact(local=dict(globals(), **locals()))
+            row = [itr, i, document_field, term_field,
+            tokenization_method.__name__, r.top1_match, r.top_5_match,
+            r.top_10_match, len(train_dataset),
+            len(query_dataset_full), query_terms_ratio]
+
+            final_df.loc[len(final_df)] = row
+
+          final_df.to_csv(output_file)
+
+
+
+#['iter', 'num_query_terms', 'document_field', 'term_field', 'strgy',
+#                              'top1_acc', 'top5_acc', 'top10_acc',
+#                              'num_train_documents', 'num_test_documents', 'query_terms_ratio']
 
 # tesoura
-import code; code.interact(local=dict(globals(), **locals()))
+# import code; code.interact(local=dict(globals(), **locals()))
